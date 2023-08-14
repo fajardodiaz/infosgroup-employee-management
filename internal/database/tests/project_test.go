3b02e9a8cef86dae76733d6be8771c111dbb309a
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-08-14 14:28:42 -0500

    Project tests ready

--- internal/database/tests/project_test.go
+++ internal/database/tests/project_test.go
@@ -1,9 +1,93 @@
 package tests
 
-// import "testing"
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
 
-// func TestCreateProject(t *testing.T) {}
-// func TestGetProject(t *testing.T)    {}
-// func TestListProjects(t *testing.T)  {}
-// func TestUpdateProject(t *testing.T)
-// func TestDeleteProject(t *testing.T) {}
+	database "github.com/fajardodiaz/infosgroup-employee-management/internal/database/sqlc"
+	"github.com/fajardodiaz/infosgroup-employee-management/internal/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomProject(t *testing.T) database.Project {
+	randomName := utils.RandomString(6)
+	project1, err := testQueries.CreateProject(context.Background(), randomName)
+	require.NoError(t, err)
+	require.NotEmpty(t, project1)
+	require.Equal(t, project1.Name, randomName)
+	require.NotZero(t, project1.ID)
+	require.NotZero(t, project1.Name)
+
+	return project1
+}
+
+func TestCreateProject(t *testing.T) {
+	CreateRandomProject(t)
+}
+
+func TestGetProject(t *testing.T) {
+	project1 := CreateRandomProject(t)
+
+	project2, err := testQueries.GetProject(context.Background(), project1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, project2)
+	require.Equal(t, project1, project2)
+	require.Equal(t, project1.ID, project2.ID)
+	require.Equal(t, project1.Name, project2.Name)
+	require.WithinDuration(t, project1.CreatedAt, project2.CreatedAt, time.Second)
+
+}
+
+func TestListProjects(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateRandomProject(t)
+	}
+
+	args := database.ListProjectsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	projects, err := testQueries.ListProjects(context.Background(), args)
+
+	require.NoError(t, err)
+	require.Len(t, projects, 5)
+
+	for _, project := range projects {
+		require.NotEmpty(t, project)
+	}
+
+}
+
+func TestUpdateProject(t *testing.T) {
+	project1 := CreateRandomProject(t)
+
+	args := database.UpdateProjectParams{
+		ID:   project1.ID,
+		Name: utils.RandomString(8),
+	}
+
+	project2, err := testQueries.UpdateProject(context.Background(), args)
+	require.NoError(t, err)
+
+	require.Equal(t, project1.ID, args.ID)
+	require.Equal(t, project2.Name, args.Name)
+
+	require.NotEqual(t, project1.Name, project2.Name)
+
+}
+
+func TestDeleteProject(t *testing.T) {
+	project1 := CreateRandomProject(t)
+
+	testQueries.DeleteProject(context.Background(), project1.ID)
+
+	project2, err := testQueries.GetProject(context.Background(), project1.ID)
+
+	require.Error(t, err)
+	require.Empty(t, project2)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+}
