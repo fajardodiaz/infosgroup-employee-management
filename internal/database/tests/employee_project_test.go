ae9215e8838ae2b11273c6b03a493860d9162de5
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-08-15 17:09:43 -0500

    AssignEmployeeToProject test ready

--- internal/database/tests/employee_project_test.go
+++ internal/database/tests/employee_project_test.go
@@ -1,6 +1,27 @@
 package tests
 
-// func TestassignEmployeeToProject(t *testing.T) database.EmployeeProject {
-// 	project1 := CreateRandomProject(t)
+import (
+	"context"
+	"testing"
 
-// }
+	database "github.com/fajardodiaz/infosgroup-employee-management/internal/database/sqlc"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAssignEmployeeToProject(t *testing.T) {
+	project1 := CreateRandomProject(t)
+	employee1 := CreateRandomEmployee(t)
+
+	args := database.AssignEmployeeToProjectParams{
+		EmployeeID: employee1.ID,
+		ProjectID:  project1.ID,
+	}
+
+	assignment, err := testQueries.AssignEmployeeToProject(context.Background(), args)
+
+	require.NoError(t, err)
+
+	require.NotEmpty(t, assignment)
+	require.Equal(t, assignment.EmployeeID, employee1.ID)
+	require.Equal(t, assignment.ProjectID, project1.ID)
+}
